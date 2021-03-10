964303257c691e1c0779d36d897f817208e7bbd9
alendro25 <57282213+alendro25@users.noreply.github.com>
2021-03-10 08:46:35 +0700

    Buat Folder dan Isinya
    Folder Layout - Header; Navbar; Footer

---
+++ app/Views/layout/layout gerald/g_footer.php
@@ -0,0 +1,94 @@
+  <!-- Gelombang Bawah -->
+  <div class="svg-wave" style="background-color: #f2f2f2">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2926A9" fill-opacity="1" d="M0,192L34.3,176C68.6,160,137,128,206,138.7C274.3,149,343,203,411,192C480,181,549,107,617,80C685.7,53,754,75,823,106.7C891.4,139,960,181,1029,208C1097.1,235,1166,245,1234,240C1302.9,235,1371,213,1406,202.7L1440,192L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+    </svg>
+  </div>
+
+
+  <!--Tentang5-->
+  <section id="tentang5">
+    <div class="tentang5">
+      <div class="container">
+        <div class="row" data-aos="fade-up" data-aos-delay="200">
+          <div class="col">
+            <div class="judul-tentang5">
+              <h1>MARI BERMITRA</h1>
+            </div>
+            <div class="isi-tentang5">
+              <p>Ketika berkaitan dengan teknologi IT untuk perusahaan, Anda membutuhkan seseorang yang ahli. Izinkan
+                kami menunjukkan kepada Anda sebuah layanan IT yang responsif, amanah, dan dapat diandalkan.</p>
+              <a href="" class="btn tombol hvr-bounce-to-top">MULAI KONSULTASI GRATIS</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section> <!-- Akhir Tentang5 -->
+
+
+
+
+  <!-- Footer -->
+  <footer id="footer">
+    <div class="footer">
+        <div class="container text-center text-md-left mt-5">
+            <div class="row">
+    
+              <div class="col-md-3 mx-auto mb-4 panjang-footer" data-aos="fade-right">
+                <h6 class="text-uppercase font-weigh-bold">Produk</h6>
+                <p class="mt-2"><a class="hvr-underline-from-left" href="#">Twins Robo App</a></p>
+              </div>
+    
+              <div class="col-md-3 mx-auto mb-4 panjang-footer" data-aos="fade-right" data-aos-delay="200">
+                <h6 class="text-uppercase font-weigh-bold">Layanan IT</h6>
+                
+                <ul class="list-unstyled">
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Sistem Informasi</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Aplikasi Mobile</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Aplikasi E-Gov</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Integrasi Sistem</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Pengadaan IT</a></li>
+                </ul>
+              </div>
+    
+              <div class="col-md-2 mx-auto mb-4 panjang-footer" data-aos="fade-right" data-aos-delay="400">
+                <h6 class="text-uppercase font-weigh-bold">Perusahaan</h6>
+    
+                <ul class="list-unstyled">
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Tentang Kami</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Pakta Integritas</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Aturan Layanan</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Kontak Kami</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Karir</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Blog</a></li>
+                </ul>
+              </div>
+    
+              <div class="col-md-3 mx-auto mb-4 panjang-footer" data-aos="fade-right" data-aos-delay="600">
+                <h6 class="text-uppercase font-weigh-bold">Akun Kami</h6>
+    
+                <ul class="list-unstyled">
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Facebook</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">Instagram</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">LinkedIn</a></li>
+                  <li class="my-2"><a class="hvr-underline-from-left" href="#">YouTube</a></li>
+                </ul>
+              </div>
+    
+            </div> <!--Akhir Row-->
+          </div>
+    </div>
+
+    <div class="footer-copyright text-center py-3">
+      <div class="container" data-aos="fade-left">
+        <br><br><br>
+        <p>Alamat : Jl. Sidosermo VI No.7, Sidosermo, Kec. Wonocolo, Kota SBY, Jawa Timur 60285 </p>
+        <p>Telepon: (031) 8437588  | Email: info@codepanstudio.com | GMAPS :  https://bit.ly/GMAPSCodepanStudio</p>
+        <br><br><br>
+      </div>
+        <p> <br> Copyright&copy;2021
+          <a href="#">CV. Arindra Production</a>
+        </p>
+      </div>
+  </footer>
---
+++ app/Views/layout/layout gerald/g_header.php
@@ -0,0 +1,47 @@
+  <!-- Header -->
+    <header>
+      <div class="container">  
+        <div class="d-flex justify-content-between row">
+          <!-- Kontak Bar -->
+          <div class="contact col-md-6 col-sm-12">
+            <div class="telepon">
+              <img src="/img/header/chat-icon.png" alt="">
+              <div class="text">
+                <p class="judul">Hubungi Kami</p>
+                <a href="tel:+6281252200089" class="isi">081252200089</a>
+              </div>
+            </div>
+
+            <div class="email">
+              <img src="/img/header/email-icon.png" alt="">
+              <div class="text">
+                <p class="judul">Email Kami</p>
+                <a href="mailto:info@codepanstudio.com" class="isi">info@codepanstudio.com</a>
+              </div>
+            </div>
+          </div>  
+
+          <!-- Icon Karir, Blog, Kontak -->
+          <ul class="d-flex justify-content-md-end justify-content-sm-start col-md-6 col-sm-12">
+            <li>
+              <a href="">
+                <img src="/img/header/karir-icon.png" alt="">
+                <p>Karir</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/img/header/blog-icon.png" alt="">
+              <p>Blog</p>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/img/header/contact-icon.png" alt="">
+              <p>Kontak</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
